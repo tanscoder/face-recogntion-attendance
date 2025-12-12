028cf0f0117465823064e75dcecfad8e3c43c51b
tanscoder <149710255+tanscoder@users.noreply.github.com>
2025-12-12 23:16:01 +0530

    Update face_recognition.py
    Adding new faces

--- face_recognition.py
+++ face_recognition.py
@@ -1,35 +1,46 @@
 import face_recognition
 import cv2
 import numpy as np
+from openpyxl import Workbook, load_workbook
+import os
+from datetime import datetime
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+def mark_attendance_excel(name):
+    file = "attendance.xlsx"
 
-# Get a reference to webcam #0 (the default one)
+    # If file doesn't exist, create with headers
+    if not os.path.exists(file):
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Attendance"
+        ws.append(["Name", "Time"])
+        wb.save(file)
+
+    # Load workbook and append
+    wb = load_workbook(file)
+    ws = wb.active
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    ws.append([name, now])
+
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+student1 = face_recognition.load_image_file("student1.jpg")
+student1_encoding = face_recognition.face_encodings(student1)[0]
 
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+student2 = face_recognition.load_image_file("student2.jpg")
+student2_encoding = face_recognition.face_encodings(student2)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
+    student1_encoding,
+    student2_encoding
 ]
 known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
+    "student1",
+    "student2"
 ]
 
 # Initialize some variables
@@ -60,18 +71,14 @@ while True:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
+            
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
 
             face_names.append(name)
+            mark_attendance_excel(name)
 
     process_this_frame = not process_this_frame
 
@@ -102,3 +109,7 @@ while True:
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
+
+
+
+
